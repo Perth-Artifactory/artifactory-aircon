879916302e26f1a96b84a7c6879f799d9a342e24
Daniel Harmsworth <atrophy@caffeinator.net>
2012-10-31 20:05:19 +0800

    New version installed at space

--- Artifactory_AC/Artifactory_AC.ino
+++ Artifactory_AC/Artifactory_AC.ino
@@ -9,7 +9,7 @@
 *  1.  Relay connected in parallel with the momentary    *
 *      SPST switch that toggles the A/C system on or off *
 *  2.  Air conditioning state indicator LED anode        *
-*      connected to the MCU ADC                          *
+*      connected to a 4n35 optocoupler                   *
 *  3.  16x2 LCD panel for status output                  *
 *  4.  SPST switch connected to MCU for +10min timer adj *
 *                                                        *
@@ -18,50 +18,48 @@
 
 #include <LiquidCrystal.h>
 
-#define acSwitchPin 13        // Pin driving the relay to control the A/C power switch
-#define acStatePin 9          // Pin connected to the A/C Power LED optocoupler
-#define uiButtonPin 8        // Pin connected to the input button
-#define backlightPin 6
+#define acSwitchPin 13          // Pin driving the relay to control the A/C power switch
+#define acStatePin 9            // Pin connected to the A/C Power LED optocoupler
+#define uiButtonPin 8           // Pin connected to the input button
+#define backlightPin 6          // Pin connected to the backlight on the LCD
 
-#define acStateThreshold 400  // Minimum ADC value for the acStatePin to be considered ON
-#define timerValue 60         // Time (in minutes) for the default timeout
-#define timerIncrement 10     // Time (in minutes) to increment the timer on button press
-#define timerMax 120          // Maximum time (in minutes) allowed to be on the timer
+#define timerValue 60           // Time (in minutes) for the default timeout
+#define timerIncrement 10       // Time (in minutes) to increment the timer on button press
+#define timerMax 120            // Maximum time (in minutes) allowed to be on the timer
 
 #define buttonDebounce 1000     // Time (in milliseconds) for debouncing the input button
-#define lcdUpdate 1000        // Time (in milliseconds) between LCD updates
+#define lcdUpdate 1000          // Time (in milliseconds) between LCD updates
 
-boolean acIsOn = false;               // AC is turned ON
-boolean acWaitingOff = false;         // AC has been sent the turn off signal
-static unsigned long acStopTime = 0;  // Time the AC will timeout in millis epoch
-static unsigned long acTimeLeft;      // Time left in millis until the timer expires
+#define backlightFadeTime 10    // Time (in milliseconds) between level changes for backlight fade
 
-// Some helpful variables to store calculated values from those set in the defines
-static unsigned long acTimeMax;
-static unsigned long timerIncrementMillis;
+boolean acIsOn = false;                 // AC is turned ON
+boolean acWaitingOff = false;           // AC has been sent the turn off signal but has not yet shut down
+static unsigned long acStopTime = 0;    // Time the AC will timeout in millis epoch
+static unsigned long acTimeLeft = 0;    // Time left in millis until the timer expires
 
-static unsigned long uiButtonDeb = 0; // Used to store the expiry time in millis epoch for the button debounce timer
+static unsigned long uiButtonDeb = 0;   // Time in millis epoch for button debounce expiry
+static unsigned long backlightFadeEnd;  // Time in millis epoch for next backlight level shift (if needed)
+int backlightTarget = 0;                // Target value for the backlight (0-255)
+int backlightVal = 0;                   // Current value of the backlight setting
+static unsigned long lcdNextUpdate;     // Time in millis epoch for next LCD refresh
+
+// Calculated values derived from defines
+static unsigned long timerMaxMillis       = timerMax * 60000;        // timerMax converted to millis
+static unsigned long timerIncrementMillis = timerIncrement * 60000;  // timerIncrement converted to millis
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-static unsigned long lcdNextUpdate;
 
 void setup ()
 {
   pinMode(acSwitchPin, OUTPUT);
   pinMode(acStatePin, INPUT);
   pinMode(uiButtonPin, INPUT);
-  //pinMode(backlightPin, OUTPUT);
+  pinMode(backlightPin, OUTPUT);
+  
   Serial.begin(9600);
   
-  acTimeMax = timerMax * 60000;
-  timerIncrementMillis = timerIncrement * 60000;
-  
   lcd.begin(16, 2);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Status: ");
-  
-  //digitalWrite(acSwitchPin, LOW);
+  lcd.clear();  
 }
 
 void loop()
@@ -71,16 +69,13 @@ void loop()
   {
     acTimeLeft = (long)( acStopTime - millis() );
   }
-  else
-  {
-    analogWrite(backlightPin, 0);
-  }
+
   // If the air con has just been turned on
   if ( ( digitalRead(acStatePin) == HIGH ) && ( !acIsOn ) )
   {
     acStopTime = millis() + ( timerValue * 60000 );
     acIsOn = true;
-    digitalWrite(backlightPin, HIGH);
+    backlightTarget = 255;
   }
   
   // If the air con has just been turned off
@@ -88,6 +83,7 @@ void loop()
   {
     acWaitingOff = false;
     acIsOn = false;
+    backlightTarget = 0;
   }
   
   // If the timer has expired
@@ -103,43 +99,50 @@ void loop()
   if ( ( (long)( millis() - uiButtonDeb ) >= 0 ) && ( digitalRead(uiButtonPin) == HIGH ) && ( acIsOn ) )
   {
     uiButtonDeb = millis() + buttonDebounce;
-    Serial.println("Button Press");
-    if ( ( acTimeLeft + timerIncrementMillis ) <= acTimeMax )
+    
+    if ( ( acTimeLeft + timerIncrementMillis ) <= timerMaxMillis )
     {
       acStopTime += timerIncrementMillis;
     }
     else
     {
-      acStopTime = millis() + ( acTimeMax );
+      acStopTime = millis() + ( timerMaxMillis );
     }
   }
   
-  // LCD Stuff
+  // LCD Refresh Function
   if ( (long)( millis() - lcdNextUpdate ) >= 0 )
   {
-    char l2[16] = "               ";
+    char lineBuff[16] = "               ";
     lcdNextUpdate += lcdUpdate;
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("System: ");
     if ( acIsOn ) { lcd.print("On "); } else { lcd.print("Off"); }
-    
+
     lcd.setCursor(0, 1);
-    
     if ( acIsOn )
     {
-      //lcd.print(acTimeLeft);
-      int mins = acTimeLeft / 60000;
-      int secs = ( acTimeLeft / 1000 ) % 60;
-      sprintf(l2,"%02d:%02d Left", mins, secs);
-      lcd.print( l2 );
+      int hours = ( acTimeLeft / 60000 ) / 60;
+      int mins =  ( acTimeLeft / 60000 ) % 60;
+      int secs =  ( acTimeLeft / 1000  ) % 60;
+      
+      sprintf(lineBuff,"%01d:%02d:%02d Left", hours, mins, secs);
+      lcd.print( lineBuff );
     }
     else
     {
-      //lcd.print(acStateVal);
-      lcd.print("System Is Off");
+      lcd.print("A/C is Off");
     }
   }
   
+  // Fader function for the backlight
+  if ( (long)( millis() - backlightFadeEnd ) >= 0 )
+  {
+    if       ( backlightVal > backlightTarget ) { backlightVal--; }
+    else if  ( backlightVal < backlightTarget ) { backlightVal++; }
+    backlightFadeEnd = millis() + backlightFadeTime;
+  }
+  analogWrite(backlightPin, backlightVal);
 }
 
